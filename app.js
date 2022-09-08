297187460d02269c0d666a08698e991c227d54b6
CoopWorldIndonesia <n.wiwinsavitri@gmail.com>
2022-09-08 16:15:03 +0800

    add process registration, bootsrap template

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const app   = express();
 
+app.use(express.static('public'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
+const path = require('path');
+const auth = require('http-auth');
 
 const router = express.Router();
+const Registration = mongoose.model('Registration');
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+});
 
 router.get('/', (req, res) => {
     res.render('form', { title: 'Registration form'});
@@ -20,7 +27,14 @@ router.post('/',
     const errors = validationResult(req);
 
     if(errors.isEmpty()) {
-        res.send('Thank you for your registration')
+        const registration = new Registration(req.body);
+
+        registration.save()
+            .then(() => { res.send('Thank you for your registration! '); })
+            .catch((err) => {
+                console.log(err);
+                res.send('Sorry! Something went wrong.')
+            })
     }else{
         res.render('form', { 
             title: 'Registration form', 
@@ -29,5 +43,14 @@ router.post('/',
         });
     }
 });
+
+router.get('/registrations', basic.check((req, res) => {
+    Registration.find()
+        .then((registrations) => {
+            res.render('index', { title: 'Listing registrations', registrations });
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.'); });
+}));
+
 
 module.exports = router;
